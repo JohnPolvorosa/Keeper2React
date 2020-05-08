b1df0a2cdff8471ae8329d163e411d07f983dd15
JohnPolvorosa <johnpolvorosa@ontariotechu.net>
2020-05-07 23:28:32 -0400

    first commit finish challenge

---
+++ notes.js
@@ -0,0 +1,64 @@
+const notes = [
+  {
+    key: 1,
+    title: "Delegation",
+    content:
+      "Q. How many programmers does it take to change a light bulb? A. None – It’s a hardware problem"
+  },
+  {
+    key: 2,
+    title: "Loops",
+    content:
+      "How to keep a programmer in the shower forever. Show him the shampoo bottle instructions: Lather. Rinse. Repeat."
+  },
+  {
+    key: 3,
+    title: "Arrays",
+    content:
+      "Q. Why did the programmer quit his job? A. Because he didn't get arrays."
+  },
+  {
+    key: 4,
+    title: "Hardware vs. Software",
+    content:
+      "What's the difference between hardware and software? You can hit your hardware with a hammer, but you can only curse at your software."
+  },
+  {
+    key: 5,
+    title: "Why does the front end developer eat alone?",
+    content:
+      "Because they didn't know how to join tables."
+  },
+  {
+    key: 6,
+    title: "Why do programmers prefer dark mode?",
+    content:
+      "Because light attracts bugs."
+  },
+  {
+    key: 7,
+    title: "Why do coders hate nature?",
+    content:
+      "Because it has too many bugs."
+  },
+  {
+    key: 8,
+    title: "How do two programmers say goodbye to each other?",
+    content:
+      "See you in a BIT."
+  },
+  {
+    key: 9,
+    title: "Why was the cellphone wearing glasses?",
+    content:
+      "Because it lost its contacts!"
+  },
+  {
+    key: 10,
+    title: "Why did the .NET developer fall asleep?",
+    content:
+      "Because they didn't like Java!"
+  }
+];
+
+export default notes;
